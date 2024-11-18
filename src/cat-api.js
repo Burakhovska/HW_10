cdf4d527ef6c6362a5184d3884c78192d40ee90f
Burakhovska Kateryna <ekaterinastavicenko@gmail.com>
2024-11-18 21:32:01 +0200

    finish

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 
  export function fetchBreeds(){
+    
     return axios.get(`${BASE_URL}breeds/`)
     .then((response) => {
             return response.data; 
@@ -12,8 +13,10 @@ const BASE_URL = 'https://api.thecatapi.com/v1/';
 }
 
 export function fetchCatByBreed(breedId){
+    
     return axios.get(`${BASE_URL}images/search?breed_ids=${breedId}`)
     .then((response) => {
+        
             return response.data; 
         })
     .catch((error) => {
